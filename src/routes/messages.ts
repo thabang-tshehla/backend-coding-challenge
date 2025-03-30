6a6528f89deae993056b1783a8b8dbc3607edb22
Thabang Tshehla <thabang.tshehla@yandex.com>
2025-03-30 14:30:08 +0200

    fix broken routes

--- src/routes/messages.ts
+++ src/routes/messages.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { getUserMessages, debugDecrypt } from '../controllers/messages';
+import { getUserMessages, debugDecrypt,postMessage } from '../controllers/messages';
 
 const router: Router = Router();
 
-router.get('/messages/:userId', getUserMessages);
+router.post('/', postMessage);
+router.get('/:userId', getUserMessages);
 router.post('/debug/decrypt', debugDecrypt);
 
 export default router;
