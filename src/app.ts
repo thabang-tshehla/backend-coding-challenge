d2ce232ee5c1242ac10d404a1fbafd0da3b6c7c0
Thabang Tshehla <thabang.tshehla@yandex.com>
2025-03-30 14:25:11 +0200

    use error handling middleware

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { type Express } from 'express';
 import userRoutes from './routes/users';
 import messageRoutes from './routes/messages';
 import { authenticate } from './middleware/auth';
+import errorHandler from './middleware/error';
 
 const app: Express = express();
 
@@ -9,6 +10,7 @@ app.use(express.json());
 
 app.use('/users', userRoutes);
 app.use('/messages', authenticate, messageRoutes);
+app.use(errorHandler);
 
 const PORT: number = parseInt(process.env.PORT || '3000', 10);
 app.listen(PORT, () => {
