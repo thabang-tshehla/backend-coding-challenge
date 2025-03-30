59fd08160010dcb7c73bf6cea08494939d201afc
Thabang Tshehla <thabang.tshehla@yandex.com>
2025-03-30 14:34:33 +0200

    dedicate route for decrypt

--- src/routes/messages.ts
+++ src/routes/messages.ts
@@ -1,10 +1,9 @@
 import { Router } from 'express';
-import { getUserMessages, debugDecrypt,postMessage } from '../controllers/messages';
+import { getUserMessages, postMessage } from '../controllers/messages';
 
 const router: Router = Router();
 
 router.post('/', postMessage);
 router.get('/:userId', getUserMessages);
-router.post('/debug/decrypt', debugDecrypt);
 
 export default router;
