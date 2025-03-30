b9df2835f1016d7e74bf3a8134a8078058feac74
Thabang Tshehla <thabang.tshehla@yandex.com>
2025-03-30 14:09:13 +0200

    use users route

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express, { type Express } from 'express';
-
+import userRoutes from './routes/users';
 const app: Express = express();
 
 app.use(express.json());
+
+app.use('/users', userRoutes);
 
 const PORT: number = parseInt(process.env.PORT || '3000', 10);
 app.listen(PORT, () => {
