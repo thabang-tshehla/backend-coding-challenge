67298f35f4ad08124bf184ff11a593e9a0361cc5
Thabang Tshehla <thabang.tshehla@yandex.com>
2025-03-30 14:14:08 +0200

    fix ts req type issue: extend express Request to include user object

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -3,7 +3,12 @@ import jwt from 'jsonwebtoken';
 import config from '../config';
 
 
-export const authenticate = (req: Request, res: Response, next: NextFunction): void => {
+interface AuthenticatedRequest extends Request {
+  user?: any;
+}
+
+
+export const authenticate = (req: AuthenticatedRequest, res: Response, next: NextFunction): void => {
   const token = req.headers.authorization?.split(' ')[1];
   if (!token) {
     res.status(401).json({ error: 'Unauthorized: No token provided' });
