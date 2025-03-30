fbc55e4da3633a150103e56eff0ad4e7319b748a
Thabang Tshehla <thabang.tshehla@yandex.com>
2025-03-30 14:23:44 +0200

    use messages route in app

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express, { type Express } from 'express';
 import userRoutes from './routes/users';
+import messageRoutes from './routes/messages';
+import { authenticate } from './middleware/auth';
+
 const app: Express = express();
 
 app.use(express.json());
 
 app.use('/users', userRoutes);
+app.use('/messages', authenticate, messageRoutes);
 
 const PORT: number = parseInt(process.env.PORT || '3000', 10);
 app.listen(PORT, () => {
